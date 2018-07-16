fbed73d7d9b3bc72d2863c080840f232fde29069
Kyle Lin <kylejlin.us@gmail.com>
2018-07-16 12:02:44 -0700

    Improve lighting

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import {
   Scene,
   Color,
-  OrthographicCamera,
+  PerspectiveCamera,
   WebGLRenderer,
   PlaneGeometry,
   Mesh,
@@ -11,20 +11,20 @@ import {
   TextureLoader,
   AmbientLight,
   SpotLight,
+  BoxGeometry,
 } from 'three';
 import GLTFLoader from 'three-gltf-loader';
 
 const TAU = 2 * Math.PI;
 const TURN_SPEED = 0.002;
 const MOVE_SPEED = 0.01;
-const _K = 0.05;
-const FRUS_WIDTH = window.innerWidth * _K;
-const FRUS_HEIGHT = window.innerHeight * _K;
+const SPOT_COLOR = 0xaaaaaa;
 
 const scene = new Scene();
 scene.background = new Color(0x005588);
-const camera = new OrthographicCamera(FRUS_WIDTH / - 2, FRUS_WIDTH / 2, FRUS_HEIGHT / 2, FRUS_HEIGHT / - 2, 1, 1000 );
+const camera = new PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 1000);
 const renderer = new WebGLRenderer();
+renderer.physicallyCorrectLights = true;
 renderer.setSize(window.innerWidth, window.innerHeight);
 window.addEventListener('resize', () => {
   camera.aspect = window.innerWidth / window.innerHeight;
@@ -62,23 +62,38 @@ window.addEventListener('keyup', (e) => {
   keys[keyName] = false;
 });
 
-const amb = new AmbientLight(0xffffff);
-scene.add(amb);
+const spot1 = new SpotLight(SPOT_COLOR);
+const spot2 = new SpotLight(SPOT_COLOR);
+const spot3 = new SpotLight(SPOT_COLOR);
+const spot4 = new SpotLight(SPOT_COLOR);
+scene.add(spot1);
+scene.add(spot2);
+scene.add(spot3);
+scene.add(spot4);
+spot1.power = spot2.power = spot3.power = spot4.power = 8 * Math.PI;
+spot1.position.set(50, 10, 50);
+spot2.position.set(-50, 10, 50);
+spot3.position.set(-50, 10, -50);
+spot4.position.set(50, 10, -50);
+spot1.lookAt(0, 0, 0);
+spot2.lookAt(0, 0, 0);
+spot3.lookAt(0, 0, 0);
+spot4.lookAt(0, 0, 0);
+//spot1.power = 8 * Math.PI;
 
-const spot = new SpotLight(0xffffff);
-spot.position.set(0, 20, 0);
-spot.power = 8 * Math.PI;
-scene.add(spot);
 
+const floorMat = new MeshStandardMaterial({ color: 0x442200 });
+floorMat.metalness = 0.0;
+floorMat.roughness = 0.8;
 const floor = new Mesh(
   new PlaneGeometry(100, 100, 50, 50),
-  new MeshBasicMaterial({ color: 0x888888 })
+  floorMat
 );
 floor.rotation.x -= 0.25 * TAU;
 scene.add(floor);
 
 let tankScene = null;
-(new GLTFLoader()).load('./models/tank.glb', (gltf) => {
+(new GLTFLoader()).load('./models/tank4.glb', (gltf) => {
   tankScene = gltf.scene;
   tankScene.position.y = 1;
   scene.add(tankScene);
@@ -100,7 +115,7 @@ const update = (dt) => {
       tankScene.position.x -= Math.sin(tankScene.rotation.y) * MOVE_SPEED * dt;
       tankScene.position.z -= Math.cos(tankScene.rotation.y) * MOVE_SPEED * dt;
     }
-    camera.position.set(tankScene.position.x + 10, tankScene.position.y + 10, tankScene.position.z + 10);
+    camera.position.set(tankScene.position.x + 25, tankScene.position.y + 25, tankScene.position.z + 25);
     camera.lookAt(tankScene.position);
   }
 };
