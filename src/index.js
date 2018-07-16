7f7690e9ee24b3655adba21153b74c8e43bf6c18
Kyle Lin <kylejlin.us@gmail.com>
2018-07-16 11:08:29 -0700

    Get the tank to show up

---
+++ src/index.js
@@ -0,0 +1,126 @@
+import {
+  Scene,
+  Color,
+  OrthographicCamera,
+  WebGLRenderer,
+  PlaneGeometry,
+  Mesh,
+  MeshStandardMaterial,
+  MeshLambertMaterial,
+  MeshBasicMaterial,
+  TextureLoader,
+  AmbientLight,
+  SpotLight,
+} from 'three';
+import GLTFLoader from 'three-gltf-loader';
+
+const TAU = 2 * Math.PI;
+const TURN_SPEED = 0.002;
+const MOVE_SPEED = 0.01;
+const FRUS_WIDTH = 20;
+const FRUS_HEIGHT = 20;
+
+const scene = new Scene();
+scene.background = new Color(0x005588);
+const camera = new OrthographicCamera(FRUS_WIDTH / - 2, FRUS_WIDTH / 2, FRUS_HEIGHT / 2, FRUS_HEIGHT / - 2, 1, 1000 );
+camera.position.set(10, 10, 10);
+camera.lookAt(0, 1, 0);
+const renderer = new WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+window.addEventListener('resize', () => {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+});
+document.body.appendChild(renderer.domElement);
+
+const keys = {};
+const Codes = {
+  38: 'UP',
+  40: 'DOWN',
+  37: 'LEFT',
+  39: 'RIGHT',
+  87: 'W',
+  65: 'A',
+  83: 'S',
+  68: 'D',
+  81: 'Q',
+  69: 'E',
+  32: 'SPACE',
+};
+window.addEventListener('keydown', (e) => {
+  const keyName = Codes[e.keyCode];
+  if (keyName === undefined) {
+    return;
+  }
+  keys[keyName] = true;
+});
+window.addEventListener('keyup', (e) => {
+  const keyName = Codes[e.keyCode];
+  if (keyName === undefined) {
+    return;
+  }
+  keys[keyName] = false;
+});
+
+const amb = new AmbientLight(0xffffff);
+scene.add(amb);
+
+const spot = new SpotLight(0xffffff);
+spot.position.set(0, 20, 0);
+spot.power = 8 * Math.PI;
+scene.add(spot);
+
+const floor = new Mesh(
+  new PlaneGeometry(100, 100, 50, 50),
+  new MeshBasicMaterial({ color: 0x888888 })
+);
+floor.rotation.x -= 0.25 * TAU;
+scene.add(floor);
+
+let tankScene = null;
+(new GLTFLoader()).load('./models/tank.glb', (gltf) => {
+  tankScene = gltf.scene;
+  tankScene.position.y = 1;
+  scene.add(tankScene);
+});
+
+const update = (dt) => {
+  if (tankScene) {
+    if (keys.LEFT) {
+      tankScene.rotation.y += TURN_SPEED * dt;
+    }
+    if (keys.RIGHT) {
+      tankScene.rotation.y -= TURN_SPEED * dt;
+    }
+    if (keys.W) {
+      tankScene.position.z -= MOVE_SPEED * dt;
+    }
+    if (keys.S) {
+      tankScene.position.z += MOVE_SPEED * dt;
+    }
+    if (keys.A) {
+      tankScene.position.x -= MOVE_SPEED * dt;
+    }
+    if (keys.D) {
+      tankScene.position.x += MOVE_SPEED * dt;
+    }
+    if (keys.Q) {
+      tankScene.position.y += MOVE_SPEED * dt;
+    }
+    if (keys.E) {
+      tankScene.position.y -= MOVE_SPEED * dt;
+    }
+  }
+};
+
+let then = Date.now();
+const loop = () => {
+  const now = Date.now();
+  const dt = now - then;
+  then = now;
+  requestAnimationFrame(loop);
+  update(dt);
+  renderer.render(scene, camera);
+};
+loop();
---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const path = require('path');
+//const SWPrecacheWebpackPlugin = require('sw-precache-webpack-plugin');
+
+module.exports = {
+  entry: ['babel-polyfill', './src/index.js'],
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'build')
+  },
+  mode: "production",
+  module: {
+    rules: [
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: [
+          'file-loader'
+        ]
+      },
+      {
+      test: /\.js$/,
+      exclude: /(node_modules|bower_components)/,
+      use: {
+        loader: 'babel-loader',
+        options: {
+          presets: ['@babel/preset-env'],
+        }
+      }
+    }
+    ]
+  },
+  /*plugins: [
+    new SWPrecacheWebpackPlugin({
+      staticFileGlobs: ['public/index.html', 'public/index.css'],
+      mergeStaticsConfig: true,
+      stripPrefix: 'public/'
+    }),
+  ]*/
+};
