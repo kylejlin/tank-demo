d06cd376d96910e1f33a092f71cc00c17bcb55bc
Kyle Lin <kylejlin.us@gmail.com>
2018-08-01 20:59:33 -0700

    Add rifle muzzle flash and smoke

--- src/systems/soldierSystem.js
+++ src/systems/soldierSystem.js
@@ -1,4 +1,5 @@
 import { System } from 'becs';
+import { Vector3, Euler } from 'three';
 import { Howl } from 'howler';
 // http://soundbible.com/1998-Gun-Fire.html
 import rifleSrc from '../audio/rifle.mp3';
@@ -38,6 +39,8 @@ const playRunAnim = (ent) => {
   ent.Soldier.fireAction.weight = 0;
 };
 
+const GUN_ANGLE_OFFSET = -0.1;
+
 const soldierSystem = new System(
   [
     ['Soldier', 'Position', 'Rotation'],
@@ -63,7 +66,7 @@ const soldierSystem = new System(
 
       if (ent.Soldier.state === 'CROUCHED') {
         if (dist < ent.Soldier.firingRange) {
-          const theta = Math.atan2(dx, dz);
+          const theta = Math.atan2(dx, dz) + GUN_ANGLE_OFFSET;
           if (ent.Rotation.y !== theta) {
             ent.Soldier.state = 'RISING';
             playRiseAnim(ent);
@@ -72,6 +75,61 @@ const soldierSystem = new System(
               ent.Soldier.currentFireCooldown = ent.Soldier.fireCooldown;
               ent.Soldier.state = 'FIRING';
               playFireAnim(ent);
+              rifleSound.play();
+              scene.addEntity({
+                Explosion: {
+                  position: ent.ThreeScene.scene.position.clone().add(
+                    new Vector3(-0.1, 7, 6.3).multiplyScalar(0.5).applyEuler(ent.ThreeScene.scene.rotation)
+                  ),
+                	positionRandomness: .1,
+                	velocity: new Vector3(0, 0, 1.375).applyEuler(new Euler(0, ent.Rotation.y + 0.25, 0)),
+                	velocityRandomness: .0,
+                	color: 0xaa4400,
+                	colorRandomness: .1,
+                	turbulence: .0,
+                	lifetime: 0.1,
+                	size: 3,
+                	sizeRandomness: 1,
+                  spawnRate: 2500,
+                  emissionDuration: 0.05,
+                },
+              });
+              scene.addEntity({
+                Explosion: {
+                  position: ent.ThreeScene.scene.position.clone().add(
+                    new Vector3(-0.1, 7, 6.3).multiplyScalar(0.5).applyEuler(ent.ThreeScene.scene.rotation)
+                  ),
+                	positionRandomness: .15,
+                	velocity: new Vector3(0, 0, 0.25).applyEuler(new Euler(0, ent.Rotation.y + 0.25, 0)),
+                	velocityRandomness: .15,
+                	color: 0x080808,
+                	colorRandomness: .0,
+                	turbulence: .0,
+                	lifetime: 0.7,
+                	size: 3,
+                	sizeRandomness: 3,
+                  spawnRate: 2500,
+                  emissionDuration: 0.1,
+                },
+              });
+              scene.addEntity({
+                Explosion: {
+                  position: ent.ThreeScene.scene.position.clone().add(
+                    new Vector3(-0.1, 7, 6.3).multiplyScalar(0.5).applyEuler(ent.ThreeScene.scene.rotation)
+                  ),
+                	positionRandomness: .05,
+                	velocity: new Vector3(0, 0, 0.5).applyEuler(new Euler(0, ent.Rotation.y + 0.25, 0)),
+                	velocityRandomness: .15,
+                	color: 0x080808,
+                	colorRandomness: .0,
+                	turbulence: .0,
+                	lifetime: 0.5,
+                	size: 3,
+                	sizeRandomness: 3,
+                  spawnRate: 1250,
+                  emissionDuration: 0.1,
+                },
+              });
             }
           }
         } else if (dist < ent.Soldier.seeingRange) {
@@ -80,7 +138,7 @@ const soldierSystem = new System(
         }
       } else if (ent.Soldier.state === 'RUNNING') {
         ent.Soldier.runAction.play();
-        const theta = Math.atan2(dx, dz);
+        const theta = Math.atan2(dx, dz) + GUN_ANGLE_OFFSET;
         ent.Rotation.y = theta;
 
         if (dist > ent.Soldier.firingRange) {
